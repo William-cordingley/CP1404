9d5d2e5403ee0eafccd772936691f8903a8b0fbb
William-cordingley <41682317+William-cordingley@users.noreply.github.com>
2018-08-14 14:49:06 +1000

    Adding files from prac 1 to prac 3

--- Practical_02/Expectation_to_complete.py
+++
@@ -1,9 +0,0 @@
-finished = False
-result = 0
-while not finished:
-    try:
-        result = int(input("Enter a Valid Integer: "))
-        finished = True
-    except ValueError:
-        print("Please enter a valid integer.")
-print("Valid result is:", result)
