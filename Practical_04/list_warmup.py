274423c95acb5aa96888affbe4e6c3cd25d0d58e
William-cordingley <41682317+William-cordingley@users.noreply.github.com>
2018-08-21 13:23:03 +1000

    prac 4 added

---
+++ Practical_04/list_warmup.py
@@ -0,0 +1,10 @@
+number = [3, 1, 4, 1, 5, 9, 2]
+print(number[0])
+#print([number[-1])
+print(number[3])
+print(number[:-1])
+print(number[3:4])
+print(5 in number)
+print(7 in number)
+print("3" in number)
+print(number + [6, 5, 3])
